01995bc66d123f83f3be69a612eef37c5fcf8637
Hatem Khabir <113791055+HatemKhabir@users.noreply.github.com>
2025-08-27 17:30:28 +0200

    Delete backend directory

--- backend/routes/auth/AuthRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { login, register } = require('../../controller/AuthController');
-const authRoutes = express.Router();
-
-router.post('/login',login)
-router.post('/register',register)
-
-
-module.exports = authRoutes;
--- backend/routes/user/UserRoutes.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const { uploadFile } = require("../../controller/UserController");
-const userRoutes=express.Router();
-
-router.post("/upload-file",uploadFile);
-
-module.exports=userRoutes;
